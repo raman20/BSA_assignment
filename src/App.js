72db9b86f6033d2b0520dc6bb54d4237e7d296d1
raman20 <ramandwivedi20@protonmail.com>
2021-10-25 16:17:43 +0530

    updated App file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+//import DataInput from "./components/data_input/data_input";
+import Chart from "./components/chart/chart";
+import Map from "./components/map/map";
+import CountrySelector from "./components/data_input/country_selector";
+import ParamSelector from "./components/data_input/param_selector";
+import TimePeriod from "./components/data_input/time_period";
+let DATA = require("./final_data.json");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countrySet, setCountrySet] = useState(new Set());
+    const [paramSet, setParamSet] = useState(new Set());
+    const [startYear, setStartYear] = useState(1990);
+    const [endYear, setEndYear] = useState(1990);
+
+    function addCountry(countryName) {
+        setCountrySet(
+            (prevCountrySet) => new Set(prevCountrySet.add(countryName))
+        );
+    }
+
+    function removeCountry(countryName) {
+        setCountrySet(
+            (prevCountrySet) =>
+                new Set(
+                    [...prevCountrySet].filter((elem) => elem !== countryName)
+                )
+        );
+    }
+
+    function addParam(param) {
+        setParamSet((prevParamSet) => new Set(prevParamSet.add(param)));
+    }
+
+    function removeParam(param) {
+        setParamSet(
+            (prevParamSet) =>
+                new Set([...prevParamSet].filter((elem) => elem !== param))
+        );
+    }
+
+    /*let chartMapData = {
+        DATA: DATA,
+        countryList: Array.from(countrySet),
+        paramList: Array.from(paramSet),
+        startYear: startYear,
+        endYear: endYear,
+    };
+
+    let countrySelectorProps = {
+        countrySet: countrySet,
+        removeCountry: removeCountry,
+        addCountry: addCountry,
+    };
+
+    let paramSelectorProps = {
+        paramSet: paramSet,
+        removeParam: removeParam,
+        addParam: addParam,
+    };
+
+    let timePeriodProps = {
+        setStartYear: setStartYear,
+        setEndYear: setEndYear,
+    };*/
+
+    return (
+        <div className="App">
+            {/*<DataInput
+                countrySelectorProps={countrySelectorProps}
+                paramSelectorProps={paramSelectorProps}
+                timePeriodSelectorProps={timePeriodProps}
+            />*/}
+            <div>
+                <CountrySelector
+                    countrySet={countrySet}
+                    addCountry={addCountry}
+                    removeCountry={removeCountry}
+                />
+                <ParamSelector
+                    paramSet={paramSet}
+                    addParam={addParam}
+                    removeParam={removeParam}
+                />
+                <TimePeriod
+                    setStartYear={setStartYear}
+                    setEndYear={setEndYear}
+                />
+            </div>
+            <Chart
+                DATA={DATA}
+                countryList={Array.from(countrySet)}
+                paramList={Array.from(paramSet)}
+                startYear={startYear}
+                endYear={endYear}
+            />
+            <Map
+                DATA={DATA}
+                countryList={Array.from(countrySet)}
+                paramList={Array.from(paramSet)}
+                startYear={startYear}
+                endYear={endYear}
+            />
+        </div>
+    );
 }
 
 export default App;
